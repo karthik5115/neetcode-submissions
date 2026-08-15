bee0221a7d15e27ec08a5e42480048c01515a90b
karthik <85474056+karthik5115@users.noreply.github.com>
2026-08-15 10:46:58 +0530

    Add: simplify-path - submission-4

---
+++ Data Structures & Algorithms/simplify-path/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String simplifyPath(String path) {
+        path+="/";
+        int x = 0,n=path.length();
+        Stack<String> stk = new Stack<>();
+        String fn = "";
+        while(x<n){
+            if(path.charAt(x)=='/'){
+                if(fn!=""){
+                    if(fn.equals("..")){
+                        if(!stk.isEmpty()){
+                            stk.pop();
+                        }
+                    }
+                    else if(!fn.equals(".")){
+                        stk.push(fn);
+                    }
+                        fn="";
+                }
+                
+            }
+            else{
+                    fn+=path.charAt(x);
+                }
+            
+                x++;
+        }
+        String ans = "";
+        if(stk.isEmpty()){
+            return "/";
+        }
+        while(!stk.isEmpty()){
+            ans = "/"+stk.pop()+ans; 
+        }
+        return ans;
+    }
+}
